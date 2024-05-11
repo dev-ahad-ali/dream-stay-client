4fb4348f8366b30cba59590882ec31b8bb439ba4
dev-ahad-ali <ahadali.dev@gmail.com>
2024-05-11 13:44:57 +0600

    register page functionality added

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,4 +1,37 @@
+import { useState } from 'react';
+import useAuth from '../Hooks/useAuth';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 const Register = () => {
+  const { createUser, logOut, updateUserProfile } = useAuth();
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const { Email, Password, PhotoURL, Name } = data;
+    // crate user and add photo and name
+    createUser(Email, Password).then(() => {
+      updateUserProfile(Name, PhotoURL).then(() => {
+        toast.success('Account created successfully,Login Now!');
+        reset();
+        logOut();
+      });
+    });
+  };
+
+  // Show hide password
+  const handleShow = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className='mt-[120px] flex items-center justify-center pb-8'>
       <div className='mx-auto max-w-7xl px-5'>
@@ -6,7 +39,7 @@ const Register = () => {
           <h2 className='font-ooh-baby text-center text-4xl font-bold'>
             Create a new account
           </h2>
-          <form onSubmit={''} className='mt-8'>
+          <form onSubmit={handleSubmit(onSubmit)} className='mt-8'>
             <div className='mt-4'>
               <label
                 className='mb-2 block text-sm font-medium text-gray-600 '
@@ -20,7 +53,18 @@ const Register = () => {
                 name='name'
                 className='block w-full rounded-lg border bg-white px-4 py-2 text-gray-700    focus:border-gray-400 focus:outline-none  focus:ring focus:ring-gray-300 focus:ring-opacity-40'
                 type='text'
+                {...register('Name', {
+                  required: {
+                    value: true,
+                    message: 'Must enter a Name',
+                  },
+                })}
               />
+              {errors.Name && (
+                <p className='font-noto text-sm font-semibold text-red-500'>
+                  {errors.Name.message}
+                </p>
+              )}
             </div>
             <div className='mt-4'>
               <label
@@ -35,7 +79,18 @@ const Register = () => {
                 name='photo'
                 className='block w-full rounded-lg border bg-white px-4 py-2 text-gray-700    focus:border-gray-400 focus:outline-none  focus:ring focus:ring-gray-300 focus:ring-opacity-40'
                 type='text'
+                {...register('PhotoURL', {
+                  required: {
+                    value: true,
+                    message: 'Must enter a Photo Url',
+                  },
+                })}
               />
+              {errors.PhotoURL && (
+                <p className='font-noto text-sm font-semibold text-red-500'>
+                  {errors.PhotoURL.message}
+                </p>
+              )}
             </div>
             <div className='mt-4'>
               <label
@@ -50,7 +105,18 @@ const Register = () => {
                 name='email'
                 className='block w-full rounded-lg border bg-white px-4 py-2 text-gray-700    focus:border-gray-400 focus:outline-none  focus:ring focus:ring-gray-300 focus:ring-opacity-40'
                 type='email'
+                {...register('Email', {
+                  required: {
+                    value: true,
+                    message: 'Must enter an Email',
+                  },
+                })}
               />
+              {errors.Email && (
+                <p className='font-noto text-sm font-semibold text-red-500'>
+                  {errors.Email.message}
+                </p>
+              )}
             </div>
 
             <div className='mt-4'>
@@ -63,13 +129,50 @@ const Register = () => {
                 </label>
               </div>
 
-              <input
-                id='loggingPassword'
-                autoComplete='current-password'
-                name='password'
-                className='block w-full rounded-lg border bg-white px-4 py-2 text-gray-700    focus:border-gray-400 focus:outline-none  focus:ring focus:ring-gray-300 focus:ring-opacity-40'
-                type='password'
-              />
+              <div className='relative'>
+                <input
+                  id='loggingPassword'
+                  autoComplete='current-password'
+                  name='password'
+                  className='block w-full rounded-lg border bg-white px-4 py-2 text-gray-700    focus:border-gray-400 focus:outline-none  focus:ring focus:ring-gray-300 focus:ring-opacity-40'
+                  type={showPassword ? 'text' : 'password'}
+                  {...register('Password', {
+                    required: {
+                      value: true,
+                      message: 'Must enter a password',
+                    },
+                    minLength: {
+                      value: 6,
+                      message: 'Password must be 6 characters long',
+                    },
+                    validate: {
+                      includesUppercase: (v) => {
+                        const pattern = /(?=.*[A-Z])/;
+                        if (!pattern.test(v)) {
+                          return 'Password must include 1 uppercase letter';
+                        }
+                      },
+                      includeLowercase: (v) => {
+                        const pattern = /(?=.*[a-z])/;
+                        if (!pattern.test(v)) {
+                          return 'Password must include 1 lowercase letter';
+                        }
+                      },
+                    },
+                  })}
+                />
+                <span
+                  className='absolute right-2 top-1/2 -translate-y-1/2 text-xl'
+                  onClick={handleShow}
+                >
+                  {showPassword ? <FaEye /> : <FaEyeSlash />}
+                </span>
+              </div>
+              {errors.Password && (
+                <p className='font-noto text-sm font-semibold text-red-500'>
+                  {errors.Password.message}
+                </p>
+              )}
             </div>
             <div className='mt-6'>
               <button
@@ -80,6 +183,16 @@ const Register = () => {
               </button>
             </div>
           </form>
+          <div className='mt-12 grid grid-cols-3 items-center gap-2'>
+            <span className='h-[2px] bg-black'></span>
+            <Link
+              to={'/login'}
+              className='text-center font-bold text-teal-600 hover:underline'
+            >
+              {`<`} Login {`>`}
+            </Link>
+            <span className='h-[2px] bg-black'></span>
+          </div>
         </div>
       </div>
     </div>
