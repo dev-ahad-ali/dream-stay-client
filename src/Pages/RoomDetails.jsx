58025ec992f86ee1c17f843fa89debaae922f39c
dev-ahad-ali <ahadali.dev@gmail.com>
2024-05-13 04:09:02 +0600

    review shown for specific room

--- src/Pages/RoomDetails.jsx
+++ src/Pages/RoomDetails.jsx
@@ -1,16 +1,39 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import { url } from '../Utils/url';
 
 const RoomDetails = () => {
   const room = useLoaderData();
+  const [reviews, setReviews] = useState([]);
+  console.log(reviews);
+
+  useEffect(() => {
+    const getReviews = async () => {
+      try {
+        const { data } = await axios.get(`${url}/reviews/${room._id}`);
+        setReviews(data);
+      } catch {
+        (err) => console.log(err);
+      }
+    };
+    getReviews();
+  }, [room._id]);
+
   return (
     <>
       <div className='mx-auto max-w-7xl'>
         <div>
           <h2>{room?.room_name}</h2>
-          <img src={room?.image} alt='' />
+          <img className='h-72' src={room?.image} alt='' />
           <Link to={'/booking'} state={room._id}>
             <button className='btn btn-warning'>Book This Room</button>
           </Link>
+          {reviews ? (
+            reviews.map((review) => <p key={review._id}>{review.comment}</p>)
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </>
