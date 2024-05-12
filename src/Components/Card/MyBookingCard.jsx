b9d7255ac7a99deb267b7f331dfbe0095bfa54c7
dev-ahad-ali <ahadali.dev@gmail.com>
2024-05-12 20:57:15 +0600

    update date funtionality added

--- src/Components/Card/MyBookingCard.jsx
+++ src/Components/Card/MyBookingCard.jsx
@@ -1,18 +1,33 @@
+import UpdateDateModal from '../Modal/UpdateDateModal';
+
 const MyBookingCard = ({ myBooking, refetch }) => {
   const { room } = myBooking;
+
   return (
-    <div className='card h-[300px] bg-base-100 shadow-xl lg:card-side'>
-      <figure>
-        <img src={room?.image} alt='Album' />
-      </figure>
-      <div className='card-body'>
-        <h2 className='card-title'>{room?.room_name}</h2>
-        <p>Click the button to listen on Spotiwhy app.</p>
-        <div className='card-actions justify-end'>
-          <button className='btn btn-primary'>Listen</button>
+    <>
+      <div className='card h-[300px] bg-base-100 shadow-xl lg:card-side'>
+        <figure>
+          <img src={room?.image} alt='Album' />
+        </figure>
+        <div className='card-body'>
+          <h2 className='card-title'>{room?.room_name}</h2>
+          <p>{myBooking?.date}</p>
+          <div className='card-actions justify-end'>
+            <button
+              onClick={() =>
+                document.getElementById('updateDate-modal').showModal()
+              }
+              className='btn btn-warning'
+            >
+              Update Date
+            </button>
+            <button className='btn btn-error'>Cancel</button>
+          </div>
         </div>
       </div>
-    </div>
+      {/* Update Modal */}
+      <UpdateDateModal myBooking={myBooking} refetch={refetch} />
+    </>
   );
 };
 
---
+++ src/Components/Modal/UpdateDateModal.jsx
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { useState } from 'react';
+import DatePicker from 'react-date-picker';
+import { url } from '../../Utils/url';
+import { toast } from 'react-toastify';
+
+const UpdateDateModal = ({ myBooking, refetch }) => {
+  const { room, date } = myBooking;
+  const [updateDate, setUpdateDate] = useState(new Date(date));
+
+  const handleDateUpdate = async () => {
+    const dateString = updateDate.toDateString();
+
+    await axios
+      .patch(`${url}/bookings/${myBooking._id}`, { dateString })
+      .then((res) => {
+        if (res.data.modifiedCount > 0) {
+          refetch();
+          toast.success('Date updated successfully');
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <dialog id='updateDate-modal' className='modal'>
+      <div className='modal-box'>
+        <form method='dialog'>
+          {/* if there is a button in form, it will close the modal */}
+          <button className='btn btn-circle btn-ghost btn-sm absolute right-2 top-2'>
+            ✕
+          </button>
+        </form>
+        <h3 className='text-lg font-bold'>{room?.room_name}</h3>
+        <DatePicker
+          format='dd-MM-yyyy'
+          onChange={(value) => setUpdateDate(value)}
+          value={updateDate}
+        />
+        <p className='py-4'>Press ESC key or click on ✕ button to close</p>
+        <form method='dialog'>
+          <button onClick={handleDateUpdate} className='btn btn-success'>
+            Update
+          </button>
+        </form>
+      </div>
+    </dialog>
+  );
+};
+
+export default UpdateDateModal;
