9381a90d9cf52d2335dde88eaa9774847b092898
dev-ahad-ali <ahadali.dev@gmail.com>
2024-05-13 23:30:20 +0600

    review card added

---
+++ src/Components/Card/ReviewCard.jsx
@@ -0,0 +1,40 @@
+const ReviewCard = ({ review }) => {
+  const { user, ratings, comment, timeStamp } = review;
+  const stars = [];
+  for (let i = 0; i < ratings; i++) {
+    stars.push(i);
+  }
+  return (
+    <div className='flex flex-col justify-between  border-2 border-amber-300 bg-white p-4'>
+      <p className=' text-gray-500'>“{comment}”.</p>
+      <div className='mt-6'>
+        <div className='flex items-center justify-between'>
+          <h1 className='font-semibold text-blue-500'>
+            <span className='text-black'>From</span> : {user}
+          </h1>
+          <div className='flex items-center gap-3'>
+            <span className='font-medium text-black'>Ratings-{ratings} : </span>
+            <div className='flex items-center gap-1'>
+              {stars?.map((star) => (
+                <div
+                  key={star}
+                  className='mask mask-star-2 w-[20px] bg-amber-500 text-amber-500'
+                >
+                  1
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='mt-2 text-end '>
+        <span className='text-xs text-gray-500'>
+          <span className='text-black'>Posted On : </span>
+          {timeStamp}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewCard;
--- src/Pages/RoomDetails.jsx
+++ src/Pages/RoomDetails.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import { url } from '../Utils/url';
+import ReviewCard from '../Components/Card/ReviewCard';
 
 const RoomDetails = () => {
   const room = useLoaderData();
@@ -21,18 +22,22 @@ const RoomDetails = () => {
 
   return (
     <>
-      <div className='mx-auto max-w-7xl'>
+      <div className='mx-auto max-w-7xl px-5 py-12'>
         <div>
           <h2>{room?.room_name}</h2>
           <img className='h-72' src={room?.image} alt='' />
           <Link to={'/booking'} state={room._id}>
             <button className='btn btn-warning'>Book This Room</button>
           </Link>
-          {reviews ? (
-            reviews.map((review) => <p key={review._id}>{review.comment}</p>)
-          ) : (
-            <></>
-          )}
+          <div className='grid grid-cols-2 gap-3'>
+            {reviews ? (
+              reviews.map((review) => (
+                <ReviewCard key={review._id} review={review} />
+              ))
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
       </div>
     </>
