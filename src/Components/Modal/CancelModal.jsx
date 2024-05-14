cbd19363bc3b6fce26a7b06f7220d18c17eeb133
dev-ahad-ali <ahadali.dev@gmail.com>
2024-05-14 15:07:16 +0600

    cancel modal style fixed

--- src/Components/Modal/CancelModal.jsx
+++ src/Components/Modal/CancelModal.jsx
@@ -26,17 +26,25 @@ const CancelModal = ({ myBooking, refetch }) => {
 
   return (
     <dialog id={`cancel${_id}`} className='modal'>
-      <div className='modal-box'>
+      <div className='modal-box rounded-none bg-white/80 backdrop-blur-sm'>
         <form method='dialog'>
           {/* if there is a button in form, it will close the modal */}
           <button className='btn btn-circle btn-ghost btn-sm absolute right-2 top-2'>
             ✕
           </button>
         </form>
-        <h3 className='text-lg font-bold'>{room?.room_name}</h3>
-        <p className='py-4'>Are you sure you wanna cancel booking</p>
-        <form method='dialog'>
-          <button onClick={handleCancel} className='btn btn-error'>
+        <h3 className='text-lg  font-bold'>
+          Canceling Booking For :{' '}
+          <span className='text-base font-normal'>{room?.room_name}</span>
+        </h3>
+        <p className='mt-4 text-center text-sm'>
+          Are you sure you wanna cancel booking
+        </p>
+        <form method='dialog' className='mt-1 text-center'>
+          <button
+            onClick={handleCancel}
+            className='btn btn-error btn-sm rounded-none uppercase text-white'
+          >
             Yes
           </button>
         </form>
