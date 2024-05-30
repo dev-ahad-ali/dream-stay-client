2d68d0a563d9d8df1156521d2b641fdcf56e68cd
dev-ahad-ali <ahadali.dev@gmail.com>
2024-05-30 18:39:57 +0600

    tab responsive done

--- src/Components/ChooseView/ChooseViewTab.jsx
+++ src/Components/ChooseView/ChooseViewTab.jsx
@@ -3,7 +3,7 @@ import RoomCard from '../Card/RoomCard';
 const ChooseViewTab = ({ rooms }) => {
   return (
     <section>
-      <div className='mt-12 grid grid-cols-4 gap-6'>
+      <div className='mt-12 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4'>
         {rooms.map((room) => (
           <RoomCard key={room._id} room={room} />
         ))}
